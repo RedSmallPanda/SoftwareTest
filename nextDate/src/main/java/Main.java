313433140b652228b5b377bbd07c6759ea0ff192
yssjtu <635664803@qq.com>
2019-05-09 15:33:18 +0800

    NextDate功能测试

--- nextDate/src/main/java/Main.java
+++ nextDate/src/main/java/Main.java
@@ -1,10 +1,15 @@
 public class Main {
 
-	public static void main(String[] args) {
-	    Date d = new Date(2,29,2016);
+    public static void main(String[]args){
+        Date d = new Date(12,31,-1);
+        d.increment();
+        System.out.println(d.toString());
+    }
+	public static String Nextdate(int month,int day,int year) {
+	    Date d = new Date(month,day,year);
 	    Date dd = new Date(d.getMonth().getCurrentPos(), d.getDay().getCurrentPos(), d.getYear().getCurrentPos());
         dd.increment();
-        System.out.println(dd.toString());
+        return dd.toString();
     }
 
 }
