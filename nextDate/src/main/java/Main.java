d0b5b2f0ad759a4de021b34f88b577b9bf21ca7d
茅悦田 <kayaetsuden@maoyuetiandeMacBook-Pro.local>
2019-05-10 21:27:18 +0800

    Date mock test

--- nextDate/src/main/java/Main.java
+++ nextDate/src/main/java/Main.java
@@ -1,15 +1,21 @@
 public class Main {
 
     public static void main(String[]args){
-        Date d = new Date(12,31,-1);
-        d.increment();
-        System.out.println(d.toString());
+        Year y=new Year(-1);
+        Month m=new Month(12,y);
+        Day d=new Day(31,m);
+        Date date = new Date(m,d,y);
+        date.increment();
+        System.out.println(date.toString());
     }
 	public static String Nextdate(int month,int day,int year) {
-	    Date d = new Date(month,day,year);
-	    Date dd = new Date(d.getMonth().getCurrentPos(), d.getDay().getCurrentPos(), d.getYear().getCurrentPos());
-        dd.increment();
-        return dd.toString();
+        Year y=new Year(year);
+        Month m=new Month(month,y);
+        Day d=new Day(day,m);
+	    Date date = new Date(m,d,y);
+	    //Date dd = new Date(date.getMonth().getCurrentPos(), date.getDay().getCurrentPos(), date.getYear().getCurrentPos());
+        date.increment();
+        return date.toString();
     }
 
 }
