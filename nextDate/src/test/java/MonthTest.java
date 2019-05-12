837ef1cf075e8bf8e8df5892204e9b10d755d3a7
gyh <RemGYH@sjtu.edu.cn>
2019-05-12 20:15:22 +0800

    setup

--- nextDate/src/test/java/MonthTest.java
+++ nextDate/src/test/java/MonthTest.java
@@ -13,6 +13,7 @@ public class MonthTest {
     @Before
     public void setUp() throws Exception {
         year = mock(Year.class);
+        when(year.isValid()).thenReturn(true);
     }
 
     @After
@@ -21,14 +22,17 @@ public class MonthTest {
 
     @Test
     public void setMonth() {
-        // case 1
-        when(year.isValid()).thenReturn(true);
+        // case 1: set valid
         month = new Month(4, year);
         month.setMonth(5, year);
         assertEquals(month.getMonth(), 5);
-        // case 2
+        // case 2: set not valid
         try {
+            month = new Month(4, year);
+            when(month.isValid()).thenReturn(false);
             month.setMonth(13, year);
+            // can't reach here
+            fail();
         } catch (Exception e) {
             assertTrue(e instanceof IllegalArgumentException);
             assertEquals(e.toString(),
@@ -39,7 +43,6 @@ public class MonthTest {
     @Test
     public void getMonth() {
         // case 1
-        when(year.isValid()).thenReturn(true);
         month = new Month(5, year);
         assertEquals(month.getMonth(), 5);
     }
@@ -47,12 +50,12 @@ public class MonthTest {
     @Test
     public void getMonthSize() {
         // case 1
-        when(year.isValid()).thenReturn(true);
-        month = new Month(5, year);
-        assertEquals(month.getMonthSize(), 31);
+        when(year.isLeap()).thenReturn(false);
+        month = new Month(2, year);
+        assertEquals(month.getMonthSize(), 28);
         // case 2
         when(year.isLeap()).thenReturn(true);
-        month.setMonth(2, year);
+        month = new Month(2, year);
         assertEquals(month.getMonthSize(), 29);
 
     }
@@ -60,24 +63,24 @@ public class MonthTest {
     @Test
     public void increment() {
         // case 1
-        when(year.isValid()).thenReturn(true);
-        month = new Month(12, year);
-        assertFalse(month.increment());
-        // case 2
         month = new Month(1, year);
         assertTrue(month.increment());
+        assertEquals(month.getMonth(), 2);
+        // case 2
+        month = new Month(12, year);
+        assertFalse(month.increment());
     }
 
     @Test
     public void isValid() {
         // case 1
-        when(year.isValid()).thenReturn(true);
         month = new Month(5, year);
         assertTrue(month.isValid());
         // case 2
         try {
             month = new Month(5, year);
             month.setMonth(5, null);
+            fail();
         } catch (Exception e) {
             assertFalse(month.isValid());
         }
@@ -106,7 +109,6 @@ public class MonthTest {
 
     @Test
     public void equals() {
-        when(year.isValid()).thenReturn(true);
         month = new Month(5, year);
         assertEquals(month, new Month(5, year));
         assertNotEquals(month, new Month(7, year));
@@ -116,7 +118,6 @@ public class MonthTest {
 
     @Test
     public void setCurrentPos() {
-        when(year.isValid()).thenReturn(true);
         month = new Month(5, year);
         assertEquals(month.getMonth(), 5);
         month.setCurrentPos(13);
@@ -125,7 +126,6 @@ public class MonthTest {
 
     @Test
     public void getCurrentPos() {
-        when(year.isValid()).thenReturn(true);
         month = new Month(5, year);
         assertEquals(month.getCurrentPos(), 5);
         month.setCurrentPos(13);
